7359cb79a0734eb81309cc401fe56f5cf6fa9668
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-24 18:43:54 -0300

    Desenvolvimento

---
+++ components/Portfolio.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from "styled-components"
+import {BsThreeDotsVertical} from "react-icons/bs"
+import { coins } from '../static/coins';
+import Coin from "./Coin"
+
+const Portfolio = () => {
+    return  (
+        <Wrapper>
+            <Content>
+                <PortfolioTable>
+                    <TableItem>
+                        <Title>Your Assets</Title>
+                    </TableItem>
+                    <Divider />
+                    <Table>
+                        <TableItem>
+                            <TableRow>
+                                <div style= {{flex: 3}}>Name</div>
+                                <div style= {{flex: 2}}>Balance</div>
+                                <div style= {{flex: 1}}>Price</div>
+                                <div style= {{flex: 1}}>Allocation</div>
+                                <div style= {{flex: 0}}><BsThreeDotsVertical/></div>
+                            </TableRow>
+                        </TableItem>
+                        <Divider />
+                        <div>{coins.map(coin => (
+                            <div>
+                                <Coin coin={coin}/>
+                                <Divider />
+                            </div>
+                        ))}
+                        </div>
+                    </Table>
+                </PortfolioTable>
+            </Content>
+        </Wrapper>
+
+    )
+}
+
+export default Portfolio
+
+
+const Wrapper = styled.div`
+    flex: 1;
+    display:flex;
+    justify-content: center;
+`
+
+const Content = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    padding: 2rem 1rem;
+`
+
+const PortfolioTable = styled.div`
+    margin-top: 1rem;
+    border: 1px solid #282b2f;
+`
+
+const Table = styled.table`
+    width: 100%;
+`
+
+const TableRow = styled.tr`
+    width: 100%;
+    display:flex;
+    justify-content: space-between;
+
+    >th {
+        text-align: left;
+    }
+`
+
+const TableItem = styled.div`
+    padding: 1rem 2rem;
+`
+
+const Divider = styled.div`
+    border-bottom: 1px solid #282b2f
+`
+
+const Title = styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+`
--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import Header from "../components/Header"
+import styled from "styled-components"
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard = ({address}) => {
-  return <Header />;
+  return (
+    <Wrapper>
+      <Sidebar/>
+      <MainContainer>
+        <Header />
+        <Main />
+      </MainContainer>
+    </Wrapper>
+  )
 };
 
 export default Dashboard;
 
+
+const Wrapper = styled.div`
+  display:flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color:white;
+  overflow:hidden;
+`
+
+
+const MainContainer = styled.div`
+  flex: 1;
+`
