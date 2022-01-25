d26361280a6c3ef7f249320bb1ef7d3847d73656
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-25 17:10:46 -0300

    Desenvolvimento

---
+++ components/modal/Transfer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Transfer = () => {
+    return(
+        <div>A</div>
+    )
+}
+
+export default Transfer
--- components/modal/TransferModal.js
+++ components/modal/TransferModal.js
@@ -1,12 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components"
+import Transfer from './Transfer';
 
 const TransferModal = () => {
+    const [action, setAction] = useState("send")
+
+    const selectedStyle= {
+       color: "#3773f5"
+    }
+
+    const unselectedStyle={
+        border: "1px solid #282b2f"
+    }
+
+    const selectedModal = option => {
+        switch(option){
+            case "send":
+                return <Transfer/>
+            case "receive":
+                return <h2>Receive</h2>
+            default:
+                return <h2>Send</h2>
+        }
+    }
+
     return(
-        <div>
-            <p style={{color: "black"}}>send</p>
-            <p style={{color: "black"}}>receive</p>
-        </div>
+        <Wrapper>
+            <Selector>
+                <Option style={ action === "send" ? selectedStyle: unselectedStyle} onClick={() => setAction("send")}>
+                    <p>Send</p>
+
+                </Option>
+                <Option style={action === "receive" ? selectedStyle: unselectedStyle} onClick={() => setAction("receive")}>
+                   <p>Receive</p>
+                </Option>
+            </Selector>
+            <ModalMain>
+                {selectedModal(action)}
+            </ModalMain>
+            
+        </Wrapper>
     )
 }
 
 export default TransferModal;
+
+
+const Wrapper = styled.div`
+    height: 35rem;
+    width: 27rem;
+    color: white;
+    border: 1px solid #282b2f;
+    display:flex;
+    flex-direction: column;
+`
+
+const Selector = styled.div`
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 5rem;
+`
+
+const Option = styled.div`
+    height: 100%;
+    width: 100%;
+    display: grid;
+    place-items: center;
+    font-size: 1.2rem;
+    font-weight: 600;
+
+    :hover{
+        cursor:pointer;
+        background-color: #111214;
+    }
+`
+
+const ModalMain = styled.div`
+    padding: 1rem;
+    flex: 1;
+`
