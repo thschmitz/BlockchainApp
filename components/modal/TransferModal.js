65e5dec3f34a54f9a6f8611dab8d5cc2b2345ca4
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-25 16:03:23 -0300

    Desenvolvimento

---
+++ components/modal/TransferModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TransferModal = () => {
+    return(
+        <div>
+            <p style={{color: "black"}}>send</p>
+            <p style={{color: "black"}}>receive</p>
+        </div>
+    )
+}
+
+export default TransferModal;
